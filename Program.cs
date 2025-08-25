48c6854da8e0d4907e66de719ef7058a82bcb4b1
Chiblood <chi.blood6@gmail.com>
2025-08-25 17:32:19 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿string name = "Jack Taylor";
+int age = 30;
+string address = "114 Allegiance Street, Raeford, NC";
+
+Console.WriteLine($"User's name is: {name}");
+Console.WriteLine($"User's age is: {age}");
+Console.WriteLine($"User's address is: {address}");
